bb649df1d1b9b60c0c39f20ad3007262d8b419f7
Mario <mario.simunic@dci-student.org>
2024-04-26 15:28:07 +0200

    minor changes, solved DB issue, switced to postgreSQL

--- bulletin_board_project/bulletin_board/views.py
+++ bulletin_board_project/bulletin_board/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, redirect
 from .models import Post
 from .forms import PostForm
+import os
+
 
 def post_list(request):
-    posts = Post.objects.all().order_by('-created_at')
+    posts = Post.objects.all().order_by('-created_at')[:10]
+    valid_posts = []
+    for post in posts:
+        if os.path.exists(post.image.path):
+            valid_posts.append(post)
     return render(request, 'bulletin_board/post_list.html', {'posts': posts})
 
 def create_post(request):
