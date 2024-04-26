824016f3c3d7f44d2b36903228cdff070201f735
Mario <mario.simunic@dci-student.org>
2024-04-26 14:23:30 +0200

    added bb app - NOT finished

---
+++ bulletin_board_project/bulletin_board/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+    video = models.FileField(upload_to='videos/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
