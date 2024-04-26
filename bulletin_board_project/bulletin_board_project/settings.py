4bc7c4e84edbf54c64f57d9bfa451b7366f2d506
Mario <mario.simunic@dci-student.org>
2024-04-26 15:00:00 +0200

    minor changes

--- bulletin_board_project/bulletin_board_project/settings.py
+++ bulletin_board_project/bulletin_board_project/settings.py
@@ -78,13 +78,23 @@ WSGI_APPLICATION = "bulletin_board_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, 'db.sqlite3'),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "bulletin_board",
+        "USER": "postgres",
+        "PASSWORD": "Dci1234!",
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
