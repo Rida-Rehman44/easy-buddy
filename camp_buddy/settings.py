1786025cacd46ed7c654aa39964cc1541e3b524e
Ghost09a1 <ghost09a1@googlemail.com>
2024-06-04 09:35:09 +0200

    chore: Remove commented out database configuration

--- camp_buddy/settings.py
+++ camp_buddy/settings.py
@@ -120,6 +120,18 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('DB_NAME'),
+#         'USER': os.environ.get('DB_USER'),
+#         'PASSWORD': os.environ.get('DB_PASSWORD'),
+#         'HOST': os.environ.get('DB_HOST'),
+#         'PORT': os.environ.get('DB_PORT'),  # Default port for PostgreSQL is 5432
+#     }
+# }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
